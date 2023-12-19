87e28d192bd545d6f54890bbd9a21134cb0990fd
Yargo Valério <yargovale@hotmail.com>
2023-12-19 15:48:34 -0300

    indexed db

--- src/app/indexedDB/page.jsx
+++ src/app/indexedDB/page.jsx
@@ -3,7 +3,7 @@
 export default function IndexedDB() {
   let db;
 
-  const request = window.indexedDB.open("chapter", 1);
+  const request = indexedDB.open("chapter", 1);
 
   request.onerror = (event) => {
     console.log("Deu tudo errado!");
@@ -14,12 +14,36 @@ export default function IndexedDB() {
     db = request.result;
   };
 
-  request.onupgradeneeded = (event) => {
-    // Save the IDBDatabase interface
-    const db = event.target.result;
+  request.onupgradeneeded = function (event) {
+    var db = event.target.result;
 
-    // Create an objectStore for this database
-    const objectStore = db.createObjectStore("name", { keyPath: "myKey" });
+    //Object store sem key path e sem key generator
+    //Nesse exemplo vc deve informar a chave ao incluir um objeto
+    var store1 = db.createObjectStore("store1");
+    store1.add({ campo: "Valor 1" }, 1); //1 = chave do objeto
+    store1.add({ campo: "Valor 2" }, 2); //2 = chave do objeto
+
+    //Object store com key path
+    //Nesse exemplo o campo definido como chave deve possuir um valor e deve ser único
+    var store2 = db.createObjectStore("store2", { keyPath: "minhaChave" });
+    store2.add({ minhaChave: "Chave1", campo: "Valor 1" });
+    store2.add({ minhaChave: "Chave2", campo: "Valor 2" });
+
+    //Object store com key generator
+    //Nesse exemplo uma chave será criada automaticamente ao incluir um objeto
+    var store3 = db.createObjectStore("store3", { autoIncrement: true });
+    store3.add({ campo: "Valor 1" });
+    store3.add({ campo: "Valor 2" });
+
+    //Object store com key path e key generator
+    //Nesse exemplo uma chave será criada automaticamente no campo "minhaChave" ao incluir um
+    //novo objeto
+    var store4 = db.createObjectStore("store4", {
+      keyPath: "minhaChave",
+      autoIncrement: true,
+    });
+    store4.add({ campo: "Valor 1" });
+    store4.add({ campo: "Valor 2" });
   };
 
   return (
